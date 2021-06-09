85f62e2862a77e9adf5fc88404a577cf987d3a41
kavin <kavinadhi15@gmail.com>
2021-06-09 21:01:22 +0530

    final modify

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium import webdriver
-
+driver = None
 
 
 def pytest_addoption(parser):
@@ -11,6 +11,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="class")
 def browser_invoke(request):
+    global driver
     browser = request.config.getoption("--browser_name")
 
     customize = webdriver.ChromeOptions()
@@ -28,5 +29,29 @@ def browser_invoke(request):
     request.cls.driver = driver # cls.driver :: were our driver object is passed to fixtures instance [request],so once we call fixture this driver also invoke
     yield
     driver.quit()
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+def _capture_screenshot(name):
+    driver.get_screenshot_as_file(name)
 
 
